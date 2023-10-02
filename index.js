b293dfb4e60669eb41e2652234a748f3b162dc6f
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-10-02 17:41:36 -0500

    makes use of multiple arathmetic operators in Java Script

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const num3 = 24
+const num4 = 5
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(1,2,4,8,16,20)
